c0ddd18ba3a52b444b5c40bb3041bf9a18a8a5f9
Dmitriy Pereverza <dmitriy.p.ws@gmail.com>
2017-09-24 19:29:54 +0300

    UI interface.form data from accountDialog

--- forms/Controllers/accountDialogController.py
+++ forms/Controllers/accountDialogController.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
+from functools import partial
 from pathlib import Path
 
-from PyQt5 import QtWidgets
+from PyQt5 import QtWidgets, QtGui
 from PyQt5.QtGui import QPixmap
-from PyQt5.QtWidgets import QDialog, QFileDialog
+from PyQt5.QtWidgets import QDialog, QFileDialog, QMessageBox
 
 import config
 from classes.Instagram.InstaBot import InstaBot
@@ -19,95 +20,139 @@ class AccountDialogController(QtWidgets.QDialog):
         'countMedia': 'label_8',
         'addCommentFileBtn': 'toolButton',
         'commentFileName': 'label_13',
-        'addLoginListFileBtn': 'toolButton_2',
-        'addLoginListFileName': 'label_38',
-        'addHashtagListFileBtn': 'toolButton_3',
-        'addHashtagListFileName': 'label_39',
-        'addLocationListFileBtn': 'toolButton_4',
-        'addLocationListFileName': 'label_40',
-    }
-    settingsContainer = {
         'userSource': {
-            'type': '',
-            'value': '',
+            'UserList': {
+                'radio': 'radioButton_3',
+                'source_type': 'file',
+                'fileName': 'label_38',
+                'source': 'toolButton_2',
+            },
+            'Hashtags': {
+                'radio': 'radioButton_4',
+                'source_type': 'file',
+                'fileName': 'label_39',
+                'source': 'toolButton_3',
+            },
+            'Geo': {
+                'radio': 'radioButton_5',
+                'source_type': 'file',
+                'fileName': 'label_40',
+                'source': 'toolButton_4',
+            },
+            'Follows': {
+                'radio': 'radioButton',
+                'source_type': 'text',
+                'source': 'lineEdit_4',
+            },
+            'FollowedBy': {
+                'radio': 'radioButton_2',
+                'source_type': 'text',
+                'source': 'lineEdit_5',
+            },
         },
-        'like': {
-            'needLike': True,
-            'firstLike': True,
-            'limit': '',
-            'count': '',
-            'range': '',
-        },
-        'follows': {
-            'needFollow': True,
-        },
-        'comments': {
-            'needComment': True,
-            'commentFilePath': '',
-        },
-        'other': {
-            'isCycleLoop': True,
-        }
     }
+    # settingsContainer = {
+    #     'userSource': {
+    #     },
+    #     'like': {
+    #         'needLike': True,
+    #         'firstLike': True,
+    #         'limit': '',
+    #         'count': '',
+    #         'range': '',
+    #     },
+    #     'follows': {
+    #         'needFollow': True,
+    #     },
+    #     'comments': {
+    #         'needComment': True,
+    #         'commentFilePath': '',
+    #     },
+    #     'other': {
+    #         'isCycleLoop': True,
+    #     }
+    # }
     def __init__(self, login):
         super().__init__()
         self.ui = Ui_AccountDialog()
         self.ui.setupUi(self.ui)
         self.account = User(InstaBot().getUserInfoByLogin(login))
+        self.resultDialog = {}
+        self.settingsContainer = {'userSource': {}}
         self.loadAccountInfo()
         self.setInnerConnects()
 
+        self.initErrorMsg()
+
+    def initErrorMsg(self):
+        self.error_msg = QMessageBox()
+        self.error_msg.setIcon(QMessageBox.Critical)
+        self.error_msg.setWindowTitle("Error")
+        self.error_msg.setDetailedText("")
+
     def setInnerConnects(self):
-        self.ui.buttonBox.accepted.connect(self.ui.accept)
+        self.ui.buttonBox.accepted.connect(self.accept)
         self.ui.buttonBox.rejected.connect(self.ui.reject)
-        self.getAttr('addCommentFileBtn').clicked.connect(self.getCommentFile)
-        self.getAttr('addLoginListFileBtn').clicked.connect(self.getLoginListFile)
-        self.getAttr('addHashtagListFileBtn').clicked.connect(self.getHashtagListFile)
-        self.getAttr('addLocationListFileBtn').clicked.connect(self.getLocationListFile)
 
-    def getCommentFile(self):
-        fname = QFileDialog.getOpenFileName(self, 'Open file', '', "Text files (*.txt)")
-        AccountDialogController.settingsContainer['comments']['commentFilePath'] = fname[0]
-        self.setAttrText('commentFileName', Path(fname[0]).name)
+        for key, userSource in AccountDialogController.listFields['userSource'].items():
+            if userSource['source_type'] == 'file':
+                self.getAttr('userSource', key, 'source').clicked.connect(partial(self.getFile, key))
 
-    def getLoginListFile(self):
-        fname = QFileDialog.getOpenFileName(self, 'Open file', '', "Text files (*.txt)")
-        AccountDialogController.settingsContainer['userSource']['type'] = 'LoginList'
-        AccountDialogController.settingsContainer['userSource']['value'] = fname[0]
-        self.setAttrText('addLoginListFileName', Path(fname[0]).name)
-
-    def getHashtagListFile(self):
-        fname = QFileDialog.getOpenFileName(self, 'Open file', '', "Text files (*.txt)")
-        AccountDialogController.settingsContainer['userSource']['type'] = 'HashtagList'
-        AccountDialogController.settingsContainer['userSource']['value'] = fname[0]
-        self.setAttrText('addHashtagListFileName', Path(fname[0]).name)
-
-    def getLocationListFile(self):
-        fname = QFileDialog.getOpenFileName(self, 'Open file', '', "Text files (*.txt)")
-        AccountDialogController.settingsContainer['userSource']['type'] = 'LocationList'
-        AccountDialogController.settingsContainer['userSource']['value'] = fname[0]
-        self.setAttrText('addLocationListFileName', Path(fname[0]).name)
+    def accept(self):
+        self.resultDialog = self.getSettings()
+        if self.resultDialog:
+            self.ui.accept()
 
     def getData(self):
         result = self.ui.exec_()
-        data = self.getSettings()
-        return data, result == QDialog.Accepted
+        return self.resultDialog, result == QDialog.Accepted
 
     def getSettings(self):
-        return AccountDialogController.settingsContainer
+        resultDialog = {}
+
+        userSourseResult = self.getUserSourseResult()
+        if userSourseResult:
+            resultDialog['sourceUser'] = userSourseResult
+
+        return resultDialog
+
+    def getUserSourseResult(self):
+        for key, value in AccountDialogController.listFields['userSource'].items():
+            if self.getAttr('userSource', key, 'radio').isChecked():
+                if value['source_type'] == 'file':
+                    if key in self.settingsContainer['userSource']:
+                        return dict(type=key, value=self.settingsContainer['userSource'][key]['value'])
+                    self.error_msg.setText("Заполните все поля источника данных")
+                    self.error_msg.exec_()
+                elif value['source_type'] == 'text':
+                    text = self.getAttr('userSource', key, 'source').text()
+                    if text:
+                        return dict(type=key, value=text)
+                    self.error_msg.setText("Заполните все поля источника данных")
+                    self.error_msg.exec_()
+
+
+    def getFile(self, type):
+        fname = QFileDialog.getOpenFileName(self, 'Open file', '', "Text files (*.txt)")
+        self.settingsContainer['userSource'][type] = {'value': fname[0]}
+        self.setAttrText(self.getAttr('userSource', type, 'fileName'), Path(fname[0]).name)
 
     def loadAccountInfo(self):
         self.setIcon()
         self.setTitleText()
-        self.setAttrText('countFollowedBy', self.account.followsCount)
-        self.setAttrText('countFollowers', self.account.followed_by)
-        self.setAttrText('countMedia', self.account.media_count)
+        self.setAttrText(self.getAttr('countFollowedBy'), self.account.followsCount)
+        self.setAttrText(self.getAttr('countFollowers'), self.account.followed_by)
+        self.setAttrText(self.getAttr('countMedia'), self.account.media_count)
 
-    def setAttrText(self, refName, value):
-        self.getAttr(refName).setText(str(value))
+    def setAttrText(self, attr, value):
+        attr.setText(str(value))
 
-    def getAttr(self, refName):
-        return getattr(self.ui, AccountDialogController.listFields[refName])
+    def getAttr(self, *args):
+        value = AccountDialogController.listFields
+        for arg in args:
+            value = value[arg]
+
+        return getattr(self.ui, value)
 
     def setIcon(self):
         imgPath = Path('{}/img/avatars/{}.jpeg'.format(config.resourse_dir_path, self.account.username))
